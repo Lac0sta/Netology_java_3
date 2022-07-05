18d318eeff3e960e297b6954c96b9b62a18dbda1
Alexey Frolov <adv.a.frolov@gmail.com>
2022-07-05 16:24:17 +0300

    first change

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        int initial_state = 100; /* начальное состояние счета клиента */
+        int initialState = 100; /* начальное состояние счета клиента */
         int refill = 4000; /* сумма пополнения счета */
         int bonus = 100; /* количество бонусов, начисляемых за каждые 100 рублей пополнения */
-        int final_state; /* состояние счета клиента после пополнения */
-        int final_bonus; /* количество начисленных бонусов при пополнении счёта более, чем на 1000 рублей */
+        int finalState; /* состояние счета клиента после пополнения */
+        int finalBonus; /* количество начисленных бонусов при пополнении счёта более, чем на 1000 рублей */
 
 
         if (refill > 1000) {
-            final_bonus = (refill / bonus);
-            final_state = (initial_state + refill + final_bonus);
+            finalBonus = (refill / bonus);
+            finalState = (initialState + refill + finalBonus);
             System.out.println("Сумма на счёте после пополнения (руб.):");
-            System.out.println(final_state);
+            System.out.println(finalState);
             System.out.println("Количество начисленных бонусов (руб.):");
-            System.out.println(final_bonus);
+            System.out.println(finalBonus);
 
         } else {
-            final_state = (initial_state + refill);
+            finalState = (initialState + refill);
             System.out.println("Сумма на счёте после пополнения (руб.):");
-            System.out.println(final_state);
+            System.out.println(finalState);
             System.out.println("Бонусов не начислено");
         }
 
